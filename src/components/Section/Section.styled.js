89e2260b698a62a912d4f6ba13623870111aaf06
ElenaLatynina <latynina.elena.v@gmail.com>
2022-10-04 18:13:52 +0300

    Section

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 8px;
+  margin: 8px;
+  border-top: 2px solid #dddddd;
+  width: 400px;
+  border-radius: 5%;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.25);
+`;
+
+export const Title = styled.h2`
+  margin: 0;
+  padding: 8px;
+  text-align: center;
+`;
